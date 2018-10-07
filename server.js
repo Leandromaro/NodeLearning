a7ddc48683774f55758780c04c094ec705a0963f
Leandro Maro <leandro.maro@globant.com>
2018-10-06 22:05:38 -0300

    Adding first server

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var http = require("http");
+var url = require("url");
 
-function onRequest(request, response) {
-    console.log("Peticion Recibida.");
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hola Mundo");
-    response.end();
+function iniciar(route) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Peticion para " + pathname + " recibida.");
+
+        route(pathname);
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("Hola Mundo");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Servidor Iniciado.");
 }
 
-http.createServer(onRequest).listen(8888);
+exports.iniciar = iniciar;
 
-console.log("Servidor Iniciado.");
+exports.iniciar = iniciar;
